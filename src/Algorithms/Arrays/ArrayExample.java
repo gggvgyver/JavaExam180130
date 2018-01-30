2b5eb1ef71919a3b26042a5d58e55da33cc6191f
gyver <gggvgyver@gmail.com>
2018-01-31 00:34:02 +0900

    내방에서 첫 커밋 180131

---
+++ src/Algorithms/Arrays/ArrayExample.java
@@ -0,0 +1,10 @@
+package Algorithms.Arrays;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] array = {5, -11, 4, 99, 55, 23, 44};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(i + "번째 배열: " + array[i]);
+        }
+    }
+}
